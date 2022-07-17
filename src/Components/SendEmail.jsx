2e42b69fafbf9d5fc4bb9ec23e1e0b2814a7e3a5
Matheus-Hen <matheushlsilva2000@gmail.com>
2022-07-17 18:46:29 -0300

    Alteração no Axios

--- src/Components/SendEmail.jsx
+++ src/Components/SendEmail.jsx
@@ -5,19 +5,20 @@ import { useFetch } from '../Hooks/useFetch'
 const Email = () => {
     const email = useRef(null)
     const [mail, setMail] = useState('')
-    const api = useFetch(mail)
+    const {message} = useFetch(mail)
 
 
     return (
         <div>
-            <h2>Emails gravados</h2>
-            <h4>{mail}</h4>
+            <h2>Resposta da Api:</h2>
+            <h4>{message}</h4>
             <div className="input-group">
                 <label className="label">Email address</label>
                 <input ref={email} name="Email" id="Email" className="input" type="email" />
                 <button className="btn" onClick={
                     () => {
                         setMail(email.current.value)
+                        email.current.value = ''
                     }
                 }>Send Email</button>
                 <div></div></div>
--- src/Hooks/useFetch.jsx
+++ src/Hooks/useFetch.jsx
@@ -1,20 +1,29 @@
-import axios, { AxiosRequestConfig, AxiosRequestHeaders } from "axios";
-import { useEffect } from "react";
+import axios from "axios";
+import { useEffect,useState } from "react";
 const api = axios.create({
     baseURL: "http://localhost:3004/",
 })
 
 export function useFetch(mail) {
+    const [message, setMessage] = useState("");
     useEffect(() => {
-        api.post('/SendEmail', {
-            email: `${mail}`,
-        })
-            .then(function (response) {
-                console.log(response);
-            })
-            .catch(function (error) {
-                console.error(error);
-            });
-
+        if (mail != "") {
+            {
+                api.post('/SendEmail', {
+                    email: `${mail}`,
+                })
+                    .then(function (response) {
+                        setMessage(response.data.message);
+                        console.log(response);
+                    })
+                    .catch(function (error) {
+                        setMessage(error.response.data.message);
+                        console.error(error);
+                    });
+            }
+        }
+        else setMessage("");
     }, [mail])
+
+    return { message };
 }
